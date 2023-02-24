b7cc20e1fc3f06fed566914eebe3a69959dcffe6
ADITYA RAJENDRA  NARNAWARE <95174961+AdityaGitHubPro@users.noreply.github.com>
2023-02-24 19:11:33 +0530

    Update Shortest Path Using Atmost One Curved Edge.java

--- Shortest Path Using Atmost One Curved Edge.java
+++ Shortest Path Using Atmost One Curved Edge.java
@@ -3,66 +3,84 @@ Question Link: https://practice.geeksforgeeks.org/problems/e7d81a082cda6bd1e959d
 
 
 class Solution {
-  public:
-    vector<int> dijkstra(int a,int b,int n,vector<pair<int,int>> adj[])
-    {
-        vector<int> dist(n+1,1e9);
-        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
-        dist[a]=0;
-        pq.push({0,a});
+    static class Node implements Comparable<Node>{
+		int v;
+		int w;
+		Node(int v, int w){
+			this.v = v;
+			this.w = w;
+		}
+		
+		public int compareTo(Node o) {
+			return this.w - o.w;
+		}
+	}
+
+	static int shortestPath(int n, int m, int a, int b, ArrayList<ArrayList<Integer>> edges) {
+		
+		ArrayList<ArrayList<Node>> adj = new ArrayList<>();
+		for(int i = 0 ; i <= n ; i++) {
+			adj.add(new ArrayList<>());
+		}
+		
+		
+		for(int i = 0 ; i < m ; i++) {
+			int u = edges.get(i).get(0);
+			int v = edges.get(i).get(1);
+			int straightWeight = edges.get(i).get(2);
+			
+			adj.get(u).add(new Node(v, straightWeight));
+			adj.get(v).add(new Node(u, straightWeight));
+		}
+		
+		int[] dista = diskstra(adj, a, n);
+		int[] distb = diskstra(adj, b, n);
+		
+		int ans = dista[b];
+		for(int i = 0 ; i < m ; i++) {
+			int u = edges.get(i).get(0);
+			int v = edges.get(i).get(1);
+			int curvedWeight = edges.get(i).get(3);
+			
+			ans = Math.min(ans, dista[u] + distb[v] + curvedWeight);
+			ans = Math.min(ans, dista[v] + distb[u] + curvedWeight);
+		}
+		
+		if(ans == 1000000001) {
+			return -1;
+		}
+		
+		return ans;
+	}
+	
+	// Time = (V+E)logv
+	static int[] diskstra(ArrayList<ArrayList<Node>> adj, int start, int n) {
+		
+		 // sorting in asc order of src-> target dist
+        int dis[] = new int[n+1];
+        Arrays.fill(dis, 1000000001);
         
-        while(!pq.empty())
+        PriorityQueue<Node> pq = new PriorityQueue<>((x,y) -> { return x.w - y.w; }); 
+        
+        
+        dis[start]=0;
+        
+        pq.add(new Node(start,0)); //1sr para is target and 2nd para is dist from src(2) to target
+        
+        while(!pq.isEmpty())
         {
-            auto curr=pq.top();
-            pq.pop();
-            int dis=curr.first;
-            int node=curr.second;
-            
-            for(auto it:adj[node])
+            Node curr_node =pq.poll();  // VlogV
+            for(Node nei : adj.get(curr_node.v))  // ElogV
             {
-                int v=it.first;
-                int wt=it.second;
-                if(dist[v]>dist[node]+wt)
+                // u -> v if(d[v] > d[u] + w[u,v]) d[v] = d[u] + w[u,v])
+                //d[v] = dis[nei.v] , d[u] = dis[curr_node.v]  , w[u,v] = nei.w
+                if(dis[nei.v] > dis[curr_node.v] + nei.w)
                 {
-                    dist[v]=dist[node]+wt;
-                    pq.push({dist[v],v});
-                }
+                    dis[nei.v] = dis[curr_node.v] + nei.w;
+                    pq.add(new Node(nei.v , dis[nei.v])); // passing the targer and its updated wt from src(2) to target
+                }   
             }
         }
-        return dist;
-    }
-    int shortestPath(int n, int m, int a, int b, vector<vector<int>> &edges) {
-        vector<pair<int,int>> adj[n+1];
-        vector<vector<int>> curved;
-        
-        for(int i=0;i<m;i++)
-        {
-            int u=edges[i][0];
-            int v=edges[i][1];
-            int wt=edges[i][2];
-            int cwt=edges[i][3];
-            
-            adj[u].push_back({v,wt});
-            adj[v].push_back({u,wt});
-            
-            curved.push_back({u,v,cwt});
-        }
-        vector<int> dist1=dijkstra(a,b,n,adj);
-        vector<int> dist2=dijkstra(b,a,n,adj);
-        
-        int ans=dist1[b];
-        
-        for(int i=0;i<m;i++)
-        {
-            int u=curved[i][0];
-            int v=curved[i][1];
-            int cwt=curved[i][2];
-            
-            ans=min(ans,dist1[u]+cwt+dist2[v]);
-            ans=min(ans,dist1[v]+cwt+dist2[u]);
-        }
-        
-        if(ans>=1e9) return -1;
-        return ans;
-    }
+        return dis;
+	}
 };
